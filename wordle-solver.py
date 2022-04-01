f9388d54090a45fd4a51f5fceba70e193f9a8e43
Diesel-19 <aselimulgur@gmail.com>
2022-04-01 18:06:03 +0200

    added wordle-solver

---
+++ wordle-solver.py
@@ -0,0 +1,44 @@
+from operator import contains
+import re
+
+def main():
+    while True:
+        print("Give word")
+        word = input()
+        print("Give excluded letters")
+        exclude = input()
+
+        print("Give included letters")
+        include = input()
+
+        english_words = load_words(len(word))
+        for english_word in english_words:
+            if re.match(word, english_word) and hasntLetters(exclude, english_word) and hasLetters(include, english_word):
+                print(english_word)
+
+def load_words(length):
+    #with open('/home/selim/Documents/wordle-solver/words-alpha.txt') as word_file:
+    with open('/home/selim/Documents/wordle-solver/wordlist-german.txt') as word_file:
+
+        valid_words = set(word_file.read().split())
+
+    words = set()
+    for word in valid_words:
+        if len(word) == length:
+            words.add(word)
+
+    return words
+
+def hasLetters(letters, word):
+    for letter in letters:
+        if not letter in word:
+            return False
+    return True
+
+def hasntLetters(letters, word):
+    for letter in letters:
+        if letter in word:
+            return False
+    return True
+    
+main()
