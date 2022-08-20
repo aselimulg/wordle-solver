442352460d454839ba303cc0744cbc1485eae041
Selim <aselimulgur@gmail.com>
2022-08-21 01:34:00 +0200

    moved wordle_solver to import a function extracted the main function to play function added function play_on browser restructered flask files

---
+++ views.py
@@ -0,0 +1,19 @@
+from flask import render_template, request, Blueprint
+from wordle_solver import play_on_browser as pob
+
+views = Blueprint(__name__, "views")
+
+
+@views.route("/", methods=["post", "get"])
+def index():
+	return render_template('index.html')
+
+
+@views.route("/search", methods=["post", "get"])
+def search():
+	form = request.form
+	word = form["wordleword"]
+	eletters = form["excluded-letters"]
+	iletters = form["included-letters"]
+	word_list = pob(word, eletters, iletters)
+	return render_template('words.html', word_list=word_list)
---
+++ wordle_solver.py
@@ -0,0 +1,120 @@
+import re
+from pathlib import Path
+
+def main():
+    play()
+
+def play_on_browser(word, eletters, iletters):
+    all_words = load_words(len(word), "e")
+    word_list = []
+    for a_word in all_words:
+        if re.match(word, a_word) and hasntLetters(eletters, all_words, word) and hasLetters(iletters, all_words, word):
+            word_list.append(a_word)
+    return word_list
+
+
+def play():
+    print("Enter q after 'Give word' to end and c to clear the excluded letters list")
+    print("Give language (d/e)")
+    language = input()
+    exclude = ""
+    while True:
+        print("Give word")
+        word = input()
+
+        if word.lower() == "q":
+            break
+        elif word.lower() == "c":
+            exclude = ""
+            continue
+
+        print("Give excluded letters")
+        exclude += input()
+
+        print("Give included letters")
+        include = input()
+
+        english_words = load_words(len(word), language)
+        for english_word in english_words:
+            if re.match(word, english_word) and hasntLetters(exclude, english_word, word) and hasLetters(include, english_word, word):
+                print(english_word)
+
+
+def load_words(length, language):
+    currentPath = Path.cwd().parent
+    englishpath = currentPath / "wordle-solver" / "wordlists" / "wordlist-english.txt"
+    germanpath = currentPath / "wordle-solver" /"wordlists" / "wordlist-german.txt"
+
+    if language.startswith("e"):
+        with open(englishpath) as word_file:
+            valid_words = set(word_file.read().split())
+    else:
+        with open(germanpath) as word_file:
+            valid_words = set(word_file.read().split())
+
+    words = set()
+    for word in valid_words:
+        if len(word) == length:
+            words.add(word)
+
+    return words
+
+
+def hasLetters(letters, word, input_word):
+    # Split character and position pairs
+    letter_list = letters.split()
+
+    # Loop through every character position pairs
+    for letter_position in letter_list:
+        # Loop through the positions
+        for character in letter_position:
+            # If the char is a letter this is our letter
+            if character.isalpha():
+                letter = character
+                # If the included character isn't in the word except for its positions in the input_word, return false
+                pos1 = getLetterCount(word, letter)
+                pos2 = getLetterCount(input_word, letter)
+                if not pos1 > pos2:
+                    return False
+            else:
+                if word[int(character) - 1] == letter:
+                    return False
+    return True
+
+# Returns true if the word doesn't contain the letter not counting the occurances in the input_word.
+
+
+def hasntLetters(letters, word, input_word):
+    for letter in letters:
+        input_count = 0
+        count = 0
+        if letter in input_word:
+            for input_letter in input_word:
+                if letter == input_letter:
+                    input_count = input_count + 1
+            for word_letter in word:
+                if letter == word_letter:
+                    count = count + 1
+            if count > input_count:
+                return False
+            else:
+                return True
+        elif letter in word:
+            return False
+    return True
+
+# Get the occurance count of a letter in a word
+
+
+def getLetterCount(word, letter):
+    pos_list = []
+    count = 0
+    for char in word:
+        if char == letter:
+            pos_list.append(count)
+        count = count + 1
+    return len(pos_list)
+
+
+if __name__ == "__main__":
+    main()
